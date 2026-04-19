0813f6e48da80d878da35ecace26ed4356df3db7
letv1nnn <letv1n2007@icloud.com>
2026-04-19 23:26:41 +0100

    changed the config

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Layout } from './Layout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Layout />
     </BrowserRouter>
   </StrictMode>,
