ae4099dff31f00cff4ddb900e54bd16a602e78cd
letv1nnn <letv1n2007@icloud.com>
2026-04-19 23:13:32 +0100

    added simple ci and some other edits

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -6,6 +6,7 @@ export const Articles = () => {
     <>
       <div className="container">
         <h1>Articles</h1>
+        <p>I'm posting articles here. Most of them are going to be related to Rust, CS, Maths and something else.</p>
       </div>
     </>
   );
