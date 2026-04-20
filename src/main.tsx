1abaaf7cd5a7ceff3ea06fae7795da4796c9bdd1
letv1nnn <letv1n2007@icloud.com>
2026-04-20 17:09:12 +0100

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Layout } from './Layout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <Layout />
     </BrowserRouter>
   </StrictMode>,
